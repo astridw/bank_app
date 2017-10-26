34ac85c56e5103400fef7ebaa528380dab0b6857
Astrid <astridwcountee@gmail.com>
2017-10-26 12:55:58 -0500

    working on ongoing bank app

---
+++ bank_app.rb
@@ -0,0 +1,21 @@
+puts "Hello Welcome to Bank of Astrid. Please sign in by typing your name"
+name = gets.chomp
+
+puts "Hi #{name}, would you like to make a deposit?(y/n)"
+answer = gets.chomp
+
+if answer == "y"
+  "how much would you like to deposit?"
+   Deposit.new
+elsif
+  "Would you like to make a withdrawal?"
+end
+
+class deposit
+  def initialize
+    @deposit = 0.0
+  end
+   def new_deposit
+     
+   end
+end
