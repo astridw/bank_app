eb4b54e97c869924c59e7a0298a8e222849adde9
Astrid Countee <astridwcountee@gmail.com>
2017-11-02 19:51:25 -0500

    added transaction class to bank app

--- bank_app.rb
+++ bank_app.rb
@@ -1,3 +1,33 @@
+# creating transactions
+class Transaction
+  def initialize
+    @transaction = 0.0
+  end
+   def new_transaction
+     @transaction << transaction
+   end
+
+   def value
+    total = 0
+    @transaction.each do |transaction|
+      total += transaction.value
+      end
+    total
+   end
+ end
+
+ class Type
+  attr_reader :deposit, :value,:withdrawal
+
+  def initialize(deposit, withdrawal, value)
+    @deposit = deposit
+    @withdrawal = withdrawal
+    @value = value
+  end
+end
+
+# UI interaction
+
 puts "Hello Welcome to Bank of Astrid. Please sign in by typing your name"
 name = gets.chomp
 
@@ -6,16 +36,8 @@ answer = gets.chomp
 
 if answer == "y"
   "how much would you like to deposit?"
-   Deposit.new
+   Transaction.new
 elsif
   "Would you like to make a withdrawal?"
-end
-
-class deposit
-  def initialize
-    @deposit = 0.0
-  end
-   def new_deposit
-     
-   end
+  Transaction.new
 end
