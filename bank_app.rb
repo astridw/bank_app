c69182d10d2ec8a17ccc74fd3a4d5ca8e0e2148c
Astrid Countee <astridwcountee@gmail.com>
2017-11-03 18:03:22 -0500

    trying to make user_balance accessible outside the class

--- bank_app.rb
+++ bank_app.rb
@@ -12,6 +12,27 @@ user_amount = gets.chomp
 
 puts "The amount you want to #{user_input} is #{user_amount} dollars. Is this correct? (y/n)"
 
-user_balance = gets.chomp
+user_confirm = gets.chomp
 
 puts "Your current balance is #{user_balance} dollars. Would like you to make another transaction? (y/n)"
+
+
+# Handling Transactions
+
+class Transaction
+   attr_accessible :user_balance
+   
+  def user_balance
+    user_balance = 0.00
+    if user_input == "DEPOSIT"
+      #add deposit amount to account balance
+       user_balance += user_input
+    elsif user_input == "WITHDRAWAL"
+      #subtract deposit amount from account balance
+      user_balance -= user_input
+    else
+      puts "Your input could not be processed. Make sure that you type Deposit or Withdrawal. "
+    end
+    return user_balance
+  end
+end
